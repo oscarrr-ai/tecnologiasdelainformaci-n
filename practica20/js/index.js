2e09791fd029ce783e0ef16203892a9c0eff20ea
Oscar Urbina Alvarado <znap2017@gmail.com>
2025-10-17 07:46:02 -0600

    subi ultima practica del parcial 2
    subi ultima practica del parcial 2

---
+++ practica20/js/index.js
@@ -0,0 +1,24 @@
+let nombre = "Oscar";
+let apellido = "Urbina Alvarado";
+let nombreCompleto = nombre + ' '+ apellido;
+console.log(nombreCompleto);
+
+let numero = 250;
+let numero2 = 300;
+
+var numero3 = 100;
+numero4 = 50; //declaraciones antiguas
+
+console.log(numero3,numero4);
+
+let suma = numero + numero2 + numero3 + numero4;
+console.log(suma);
+
+let resta = numero3 - numero2;
+console.log('El resultado es:', resta);
+
+let multiplica = numero * numero2;
+console.log('El resultado es:', multiplica);
+
+let divide = numero3 / numero4;
+console.log('El resultado de la division es:', divide);
